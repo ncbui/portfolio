1f7d59c0f67c2801c21b5f82cbc062d28e885041
cb <29789422+ncbui@users.noreply.github.com>
2025-03-09 22:15:03 -0700

    Add canvas

--- src/components/SnakeGame/Canvas.jsx
+++ src/components/SnakeGame/Canvas.jsx
@@ -1,3 +1,29 @@
+import { useRef, useEffect } from "react";
+import { theme } from "../../template/theme";
+
+
+
+const defaultCanvas = () =>{
+    const WIDTH = 30;
+    const HEIGHT = 24;
+    const SCALE = 24;
+    const default_canvas = document.getElementById("board");
+    default_canvas.setAttribute("height", `${HEIGHT * SCALE}`);
+    default_canvas.style.marginTop = `${(HEIGHT * SCALE) / -2}px`;
+    default_canvas.setAttribute("width", `${WIDTH * SCALE}`);
+    default_canvas.style.marginLeft = `${(WIDTH * SCALE) / -2}px`;
+}
+
 export default function Canvas (props){ 
-    return(<canvas {...props}/>)
+    const canvasRef = useRef(defaultCanvas)
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        //Our first draw
+        context.fillStyle = theme.palette.primary.dark1
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+      }, [])
+
+    return <canvas ref={canvasRef} {...props}/>
 }
--- src/components/SnakeGame/SnakeGame.jsx
+++ src/components/SnakeGame/SnakeGame.jsx
@@ -12,16 +12,14 @@ export default function SnakeGame () {
             borderColor: 'pink',
             height: '90%',
             }}>
-            <Typography level="h1" variant="h1" sx={{color: theme.palette.primary.main, width: '100%', alignSelf:'center', '@media (max-width: 780px)' : {width:'20vw'}}}>
+            <Typography level="h1" variant="h1" sx={{color: theme.palette.primary.main, alignSelf:'center', '@media (max-width: 780px)' : {width:'20vw'}}}>
                 Snake
           </Typography>
           <Typography level="h5">
                 Use the arrow keys to guide your snake to food.
                 Eat well. and outlive the other snake.
           </Typography>
-          <Typography level="h3" variant="body-md">
-             <Canvas />
-          </Typography>
+        <Canvas />
         </Container>
     )
 }
