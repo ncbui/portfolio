a3ce1188bc18f6242fe5a831cff28d26645297a5
cb <29789422+ncbui@users.noreply.github.com>
2025-03-11 22:58:53 -0700

    Update names for snake props

--- src/components/SnakeGame/Snake.jsx
+++ src/components/SnakeGame/Snake.jsx
@@ -1,20 +1,19 @@
 import { theme } from "../../template/theme";
 
 export default class Snake {
-    constructor(snake, color = "orange", setSnakeState) {
-        this.parts = snake; // list of Points in snake body
+    constructor(snake, color = "orange") {
+        this.snake = snake; // list of Points in snake body
         this.color = color;
         this.dx = 10; // Horizontal velocity
         this.dy = 0; // Vertical velocity
-        this.setState = setSnakeState ; // props
       }
       //   Find the head of the snake to move it
     head() {
-    return this.parts[0];
+    return this.snake[0];
     }
 
     draw(ctx){
-        this.parts.forEach((snakePart) =>{
+        this.snake.forEach((snakePart) =>{
             ctx.fillStyle = theme.palette.primary.bright;
             // Set the border colour of the snake part
             ctx.strokestyle = theme.palette.primary.main;
@@ -26,10 +25,12 @@ export default class Snake {
             })
     }
 
-    moveSnake(){
+    move(setSnake){
+        console.log("before mpve", this.snake)
         let newHead = {x: this.snake[0].x + this.dx, y: this.snake[0].y + this.dy};
-        this.parts.unshift(newHead);
-        this.parts.pop();
-        this.setSnakeState(this.snake);
+        this.snake.unshift(newHead);
+        this.snake.pop();
+        console.log("after mpve", this.snake)
+        setSnake(this);
     }
 }
