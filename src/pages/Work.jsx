b27c9abf51051c2dc88baea85a27efedea410ca9
cb <29789422+ncbui@users.noreply.github.com>
2025-03-17 01:41:06 -0700

    Add Worksheets

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,10 +1,11 @@
-import { BodySheets } from '../template/theme';
+import { WorkSheets } from '../template/theme';
 import WorkExperience from '../components/WorkExperience';
+
 
 export default function Work() {
   return (
-    <BodySheets sx={{width: '786px', '@media (max-width: 780px)' : {width:'100%'}}}>
+    <WorkSheets>
       <WorkExperience />
-    </BodySheets>
+    </WorkSheets>
   );
 }
