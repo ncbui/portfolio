232630dba2381236ce2f227831e62c0c10a0f58c
cb <29789422+ncbui@users.noreply.github.com>
2025-03-17 00:25:11 -0700

    Move theme and style into app; error boundary around app

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,45 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from '@mui/joy';
-import ErrorBoundary from "./components/ErrorBoundary";
 import NavMenu from './components/NavMenu';
 import Home from './pages/Home';
 import Work from './pages/Work';
-import { theme } from "./template/theme";
 import ErrorPage from './pages/ErrorPage';
+import {CssBaseline, GlobalStyles, } from "@mui/material";
+import { ThemeProvider } from '@mui/styles';
+import { theme, global } from './template/theme';
 
 export default function App() {
   return (
-    <Container 
-      sx={{
-        display: 'flex',
-        flexDirection:'row', 
-        width: '100vw',
-        maxWidth: '968px',
-        height: '100vh',
-        overflow: 'hidden',
-        m: 0,
-        p: 0,
-        '@media (max-width: 780px)' : {
-          flexDirection:'column',
-          maxWidth: '100vw',
-          overflowX: 'hidden',
-          overflowY: 'auto',
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles styles={global} />
+      <Container 
+        sx={{
+          display: 'flex',
+          flexDirection:'row', 
+          width: '100vw',
+          maxWidth: '968px',
+          height: '100vh',
+          overflow: 'hidden',
           m: 0,
           p: 0,
-        }
-      }}
-      >
-      <ErrorBoundary>
-        <NavMenu/>  
-        <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/work" element={<Work />} />
-            <Route element={<ErrorPage />} />
-        </Routes>
-        </ErrorBoundary>
-    </Container>
+          '@media (max-width: 780px)' : {
+            flexDirection:'column',
+            maxWidth: '100vw',
+            overflowX: 'hidden',
+            overflowY: 'auto',
+            m: 0,
+            p: 0,
+          }
+        }}
+        >
+          <NavMenu/>  
+          <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/work" element={<Work />} />
+              <Route element={<ErrorPage />} />
+          </Routes>
+      </Container>
+    </ThemeProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter} from 'react-router-dom';
-import {CssBaseline, GlobalStyles, } from "@mui/material";
-import { ThemeProvider } from '@mui/styles';
-import { theme, global } from './template/theme';
+import ErrorBoundary from './components/ErrorBoundary';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <HashRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <GlobalStyles styles={global} />
+        <ErrorBoundary>
           <App />
-      </ThemeProvider>
+        </ErrorBoundary>
       </HashRouter>
   </React.StrictMode>
 );
