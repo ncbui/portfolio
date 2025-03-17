44ec73e64ebb9988ca023bb4112c2974caff9f80
cb <29789422+ncbui@users.noreply.github.com>
2025-03-17 00:31:58 -0700

    Style error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import { Typography } from '@mui/material';
+import { Container } from '@mui/joy';
+import { appContainer } from '../template/theme';
 
-export default function ErrorPage() {
+export default function ErrorPage() {   
     return (
-        < >
-        <Typography> Oops! </Typography>
+      <Container sx={appContainer}>
         <Typography> Resource unavailable at this time </Typography>
-        </>
+      </Container>
     );
 }
