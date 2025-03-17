24c752b39342d6bddbd9bae3f676b921a5244051
cb <29789422+ncbui@users.noreply.github.com>
2025-03-17 14:01:57 -0700

    Build experience table from json

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -3,127 +3,34 @@ import {Table, TableContainer, TableBody, TableCell, TableRow, List, ListItem, B
 import TurnSlightRightOutlinedIcon from '@mui/icons-material/TurnSlightRightOutlined';
 import { useTheme } from '@mui/styles';
 import { Typography } from '@mui/joy';
-import { BootstrapButton, tableRow } from '../template/theme';
-
-const experience = {
-    "work": [{
-        "position": "Consultant Software Developer",
-        "company": "Thoughtworks, Inc.",
-        "website": "https://www.thoughtworks.com",
-        "city": "remote, US ",
-        "dates": "2024-2021",
-        "summary": [
-            "Build and maintain data pipelines and platforms, across frontend, backend, and cloud infrastructure. Worked closely with cross-functional teams to enable scientists, healthcare providers, operations specialists, and more to build tools to understand their data."
-        ],
-        "acheivements": [
-            "Worked with cross-functional team to design, deploy, and maintain an pipeline that makes it easier for patient-generated data to advocate for quality patient care.",
-            "Integrated monitoring, resource deployment, standardized exception handling, and security and compliance checks, into ci/cd pipelines to reduce Time to Error Resolution for and Time to Deployment into staging for new features.",
-            "Presented relatable software & engineering concepts at lunch'n'learn series to teams of various expertises."
-            ],
-        "tools": ["Kubernetes", "ArgoCD", "PySpark", "React", "Python", "PostgreSQL", "Azure"]
-    },
-    {
-        "position": "AWS Cloud Practitioner",
-        "company": "AWS",
-        "website": "https://www.thoughtworks.com",
-        "city": " ",
-        "dates": "2021",
-        "summary": [],
-        "acheivements": [],
-        "tools": []
-    },
-        {
-        "position": "Data Processing ",
-        "company": "Invitae, Inc.",
-        "website": "https://www.invitae.com",
-        "city": "SF, US ",
-        "dates": "2020-2017",
-        "summary": ["Processed patient health records and streamline data quality checks."],
-        "acheivements": [],
-        "tools": ["EMR", "Salesforce", "RedShift",]
-    },
-    {
-        "position": "Graduate Research Assistant",
-        "company": "Action and Consciousness Lab, SFSU",
-        "website": "https://faculty.sfsu.edu/~morsella/content/action-and-consciousness-laboratory",
-        "city": "SF, US ",
-        "dates": "2019-2016",
-        "summary": ["Proposed, conducted, and analyzed behavioral experiments on automatic visual and syntactic processing."],
-        "acheivements": [],
-        "tools": ["EMR", "Salesforce", "RedShift",]
-    },
-    {
-        "position": "Health Policy Intern",
-        "company": "Health Promotion & Wellness unit, SFSU",
-        "website": "https://wellness.sfsu.edu/",
-        "city": "SF, US ",
-        "dates": "2017-2016",
-        "summary": ["Conducted focus groups for the newly-built Mashouf Wellness Center (MWC) that found that students on this commuter campus overwhelmingly needed places to rest. In 2024, MWC launched the Zen Den, a free space to nap at SFSU. "],
-        "acheivements": ["Conducted focus groups for the newly-built Mashouf Wellness Center (MWC) that found that students on this commuter campus overwhelmingly needed places to rest. Presented findings with supporting researching to the MWC student board. In 2024, MWC launched the Zen Den, a free space to nap at SFSU. "],
-        "tools": ["semi-structured interviews and focus groups","literature review"]
-    }
-],
-"certs": ["AWS Cloud Practitioner"],
-"education": [
-    {
-    "institution": "San Francisco State University",
-    "city": "San Francisco, CA",
-    "program": "Social, Personality, and Affective Psychology",
-    "degree": "Graduate coursework towards MA",
-    "dates": "2018-2016",
-    "website":"https://psychology.sfsu.edu/social-personality-and-affective-science-psychology-graduate-program"
-    },
-    {"institution": "San Francisco State University",
-    "city": "San Francisco, CA",
-    "program": "Kinesiology, concentration in Neuromuscular Science",
-    "degree": "BSc",
-    "dates": "2016-2013",
-    "website":"http://kin.sfsu.edu/undergraduate-programs#tab-content-tab-455-1"
-}
-], 
-}
-
+import { BootstrapButton, navTitle, tableRow } from '../template/theme';
+import * as resume from "../template/resume.json"
 
 export default function WorkExperience () { 
     const theme = useTheme()
 
-    const makeWorkRow = (exp) =>{
-        return( exp.map((a,i)=>{
-            return (
-            <TableRow key={a+i} sx={tableRow} >
-              <TableCell scope="row" sx={{ verticalAlign: 'top', p:0, m:0,  borderRight:'3px dotted', borderColor:theme.palette.bright, borderBottom: 0,}}>
-                      <Typography sx={{fontSize:"85%", color: theme.palette.bright}}> {a.dates} </Typography>
-                  </TableCell>
-                  <TableCell sx={{ verticalAlign: 'top', py:"0.2rem", m:0, borderBottom: 0, maxWidth: '80%', '@media (max-width: 786px)': {maxWidth: '50vw'} }}>
-                      <Link to={a.website} target="_blank" rel="noopener noreferrer">
-                        <Typography sx={{fontSize:"100%", color: theme.palette.light, }}>
-                            <b>{a.position}</b>  {a.company}
-                            <TurnSlightRightOutlinedIcon  sx={{color: theme.palette.bright, fontSize:'1rem'}}/>
-                        </Typography >
-                      </Link> 
-                      <List sx={
-                          {fontSize: '90%',
-                            width: '90%',
-                            color: theme.palette.text,
-                      }}>
-                          {listA(a.summary)}
-                          {makeButtons(a)}
-                      </List>
-                  </TableCell>    
-            </TableRow>
-        )
-    }))
-    }
-
-    
-
-    const listA = (acheivements) => {
-        return (
-            <>
-            {acheivements.map((a,i)=>{return <ListItem key={i} sx={{color:'inherit', fontFamily:'Hind'}}>{a}</ListItem>})}
-            </>
-        )
-    }
+    const makeWorkRow=(work)=>{
+      return( 
+        work.map((w)=>{
+          return (
+          <TableRow key={w.endDate} sx={tableRow} >
+            <TableCell scope="row" sx={{ verticalAlign: 'top', p:0, m:0,  borderRight:'3px dotted', borderColor:theme.palette.bright, borderBottom: 0,}}>
+              <Typography sx={{fontSize:"85%", color: theme.palette.bright, fontWeight: 'bold'}}> {w.startDate} - {w.endDate}</Typography>
+            </TableCell>
+            <TableCell sx={{ verticalAlign: 'top', py:"0.2rem", m:0, borderBottom: 0, maxWidth: '80%', '@media (max-width: 786px)': {maxWidth: '50vw'} }}>
+              <Link to={w.website} target="_blank" rel="noopener noreferrer">
+                <Typography sx={{fontSize:"100%", color: theme.palette.light, }}>
+                    {w.position}  {w.company}
+                    <TurnSlightRightOutlinedIcon  sx={{color: theme.palette.bright, fontSize:'1rem'}}/>
+                </Typography >
+              </Link> 
+              <List sx={{fontSize: '1rem',width: '90%', color: theme.palette.text,}}>
+              <ListItem sx={{color:'inherit', fontFamily:'Hind'}}>{w.summary}</ListItem>
+                {makeButtons(w)}
+              </List>
+            </TableCell>
+          </TableRow>
+      )}))}
 
     const makeButtons = ({tools}) =>{
         return( 
@@ -140,39 +47,71 @@ export default function WorkExperience () {
             </> 
         )}
 
-
     const makeEduRow = (exp) =>{
-      return( exp.map((a,i)=>{
+      return( exp.map((a)=>{
           return (
-          <TableRow key={a+i} sx={tableRow} >
-              <TableCell scope="row" sx={{ horizontalAlign: 'right', verticalAlign: 'top', p:0, m:0,  borderRight:'3px dotted', borderColor:theme.palette.bright, borderBottom: 0,}}>
-                <Typography sx={{fontSize:"85%", color: theme.palette.bright}}> {a.dates} </Typography>
+          <TableRow key={a.endDate} sx={tableRow} >
+              <TableCell scope="row" sx={{ 
+                horizontalAlign: 'right', verticalAlign: 'top', p:0, m:0,  
+                borderRight:'3px dotted', borderColor:theme.palette.bright,  
+                fontFamily: 'Hind', borderBottom: 0,
+                }}>
+                <Typography sx={{fontSize:'85%', color: theme.palette.bright, fontWeight: 'bold'}}> 
+                  {a.endDate} </Typography>
               </TableCell>
               <TableCell sx={{ verticalAlign: 'top', py:"0.2rem", m:0, borderBottom: 0, maxWidth: '80%' }}>
-                <Typography sx={{fontSize:"95%"}}>
-                  <Link to={a.website} target="_blank" rel="noopener noreferrer"><b>{a.degree} </b> {a.program} {a.institution}</Link> 
+                <Typography sx={{fontSize:"1rem, "}}>
+                  <Link to={a.website} target="_blank" rel="noopener noreferrer">{a.studyType}  {a.area}. {a.institution}</Link> 
                   <TurnSlightRightOutlinedIcon  sx={{color: theme.palette.bright, fontSize:'80%'}}/>
                 </Typography>
               </TableCell>    
           </TableRow>
       )}))}
-  
-    const expTable = () => {
-      return (
-        <TableContainer>
-        <Table aria-label="experience table" > 
-          <TableBody>
-            {makeWorkRow(experience.work)}
-            {makeEduRow(experience.education)}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-    }
+    const makeCertRow = (cert) =>{
+      return( cert.map((a)=>{
+          return (
+          <TableRow key={a.name} sx={tableRow} >
+              <TableCell scope="row" sx={{ 
+                horizontalAlign: 'right', verticalAlign: 'top', p:0, m:0,  
+                borderRight:'3px dotted', borderColor:theme.palette.bright,  
+                fontFamily: 'Hind', borderBottom: 0,
+                }}>
+                <Typography sx={{fontSize:'85%', color: theme.palette.bright}}> 
+                  {a.date} </Typography>
+              </TableCell>
+              <TableCell sx={{ verticalAlign: 'top', py:"0.2rem", m:0, borderBottom: 0, maxWidth: '80%' }}>
+                <Typography sx={{fontSize:"1rem"}}>
+                  <Link to={a.website} target="_blank" rel="noopener noreferrer">{a.name} {a.issuer}</Link> 
+                  <TurnSlightRightOutlinedIcon  sx={{color: theme.palette.bright, fontSize:'80%'}}/>
+                </Typography>
+              </TableCell>    
+          </TableRow>
+      )}))}
+
+      const makeHeader = (section) =>{
+        return(
+          <TableRow>
+          <TableCell colSpan={2} scope="row" sx={{ horizontalAlign: 'right', verticalAlign: 'top', p:0, m:0,  borderColor:theme.palette.bright,  fontFamily: 'Tourney Variable', borderBottom: 0,}}>
+            <Typography level="h5" sx={navTitle}>
+            {section}
+            </Typography>
+          </TableCell>
+      </TableRow>
+        )
+      }
     
     return (
-        <Box sx={{ p:0}}>
-          {expTable()}
-        </Box>
+      <TableContainer>
+        <Table aria-label="work and experience" > 
+          <TableBody>
+            {makeHeader("Experience")}
+            {makeWorkRow(resume.work)}
+            {makeHeader("Certificates")}
+            {makeCertRow(resume.certificates)}
+            {makeHeader("Education")}
+            {makeEduRow(resume.education)}
+          </TableBody>
+        </Table>
+    </TableContainer>
     )
 }
